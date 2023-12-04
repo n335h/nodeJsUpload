0c2b9465daf63d03f72336105b43f13450f0b583
Nich H <nmjhorishny@gmail.com>
2023-12-04 19:26:01 +0000

    Node Js Upload Practice

---
+++ middleware/filesPayloadExist.js
@@ -0,0 +1,9 @@
+const filesPayLoadExist = (req, res, next) => {
+    if (!req.files) 
+        return res.status(400).json({ status: 'error', message: 'No files were uploaded.' });
+    
+    next();
+}
+
+module.exports = filesPayLoadExist;
+ 
